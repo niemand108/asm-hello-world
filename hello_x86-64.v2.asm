5a6fecf784bf297426a871a08d82dda845c578d9
niemand108 <santilozano@gmail.com>
2020-01-05 18:21:07 +0100

    v2

---
+++ hello_x86-64.v2.asm
@@ -0,0 +1,29 @@
+;;;  nasm -f elf64 -o hello_x86-64.v2.o hello_x86-64.v2.asm
+;;;  ld -o hello_x86-64.v2 hello_x86-64.v2.o --pic-executable  --dynamic-linker=/lib64/ld-linux-x86-64.so.2
+section .rodata
+  msg: db "Hello world!", 10
+  msglen: equ $ - msg
+
+section .data
+garbage: db "Nothing to see here.", 0h
+
+section .text
+global _start
+_start:
+subroutineWrite:
+  mov rax, 1        ;	system call number for write
+  mov rdi, 1        ;   first parama: STDOUT
+  mov rsi, msg      ;	second param
+  mov rdx, msglen   ;   third param
+  syscall           ;
+  jmp subroutineExit
+.localNop:
+  nop
+  nop
+  nop
+  jmp subroutineExit
+subroutineExit:
+  mov rax, 60       ;	system call number for exit
+  mov rdi, 0        ;   exit(0)
+  syscall           ;
+
